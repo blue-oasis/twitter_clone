ec0b13e9c7edecfcb8c10ac7b8411d2138c57629
JiWon <radiohead@knou.ac.kr>
2022-09-14 15:17:59 +0900

    데이터 화면에 표시
    getDocs(collection(dbService, "tweets"));
    근데 두번씩 돔 고쳐야함

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { dbService } from "../fbase";
+import { async } from "@firebase/util";
 
 const Home = () => {
     const [tweet, setTweet] = useState(""); //텍스트를 상태로 관리하기 위해 useState 사용
+    const [tweets, setTweets] = useState([]);
+
+    const getTweets = async () => {
+        const dbTweets = await getDocs(collection(dbService, "tweets"));
+        
+        dbTweets.forEach((document) => {
+        const tweetObject = { ...document.data(), id: document.id };
+        setTweets((prev) => [tweetObject, ...prev]) //전개 구문으로 데이터 순서대로 쌓기
+        });         //prev에 순회 이전의 상태가 넘어옴, 새 데이터와 합쳐서 저장
+    };
+
+    useEffect(() => {
+        getTweets();
+    }, []);
+
 
     const onSubmit = async (event) => { //원래의 이벤트 방지(새로고침 방지)
         event.preventDefault();
@@ -23,6 +39,7 @@ const Home = () => {
       };
 
     return (
+        <>
         <form onSubmit={onSubmit}>
             <input
                 value={tweet}
@@ -33,6 +50,14 @@ const Home = () => {
             />
             <input type="submit" value="Tweet" />
         </form>
+        <div>
+            {tweets.map((tweet) => (
+                <div key={tweet.id}>
+                    <h4>{tweet.text}</h4>
+                </div>
+            ))}
+        </div>
+        </>
     );
 };
 
